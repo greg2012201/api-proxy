0f6e0a010074dad518b0f91f1286b0f0075343e1
Grzegorz Dubiel <greg2012201@gmail.com>
2021-01-19 09:06:54 +0100

    Add speedLimiter and setup it

--- src/api/apis-data.js
+++ src/api/apis-data.js
@@ -1,23 +1,25 @@
 const express = require('express');
 const axios = require('axios');
-
 const rateLimit = require("express-rate-limit");
-
+const slowDown = require("express-slow-down");
 
 
 const limiter = rateLimit({
     windowMs: 30 * 1000,
-    max: 2
+    max: 10,
 });
-
-;
+const speedLimiter = slowDown({
+    windowMs: 30 * 1000,
+    delayAfter: 1,
+    delayMs: 500,
+});;
 const router = express.Router();
 const BASE_URL = 'https://jsonplaceholder.typicode.com/';
 
 
 let cachedData;
 let cacheTime;
-router.get('/', limiter, async (req, res, next) => {
+router.get('/', limiter, speedLimiter, async (req, res, next) => {
     if (cacheTime && cacheTime > Date.now() - 30 * 1000) res.json(cachedData)
     try {
         const params = new URLSearchParams({
